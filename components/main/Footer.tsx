f1d3544dea0be99321202c05ff7fbd5643098c69
alexander <alex43bg@gmail.com>
2024-02-13 14:44:46 +0200

    1start

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -58,13 +58,13 @@ const Footer = () => {
                     </p>
                     <p className="flex flex-row items-center my-[15px] cursor-pointer">
                   
-                        <span className="text-[15px] ml-[6px]">mifwebchain@gmail.com</span>    
+                        <span className="text-[15px] ml-[6px]">mail@gmail.com</span>    
                     </p>
                 </div>
             </div>
 
             <div className="mb-[20px] text-[15px] text-center">
-                &copy; WebChain Dev 2023 Inc. All rights reserved
+                &copy; Alex Dev 2024
             </div>
         </div>
     </div>
